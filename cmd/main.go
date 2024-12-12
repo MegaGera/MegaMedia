304fff655f7ee8d7996514c6b529310945b3b5fe
MegaGera <gera1397@gmail.com>
2024-12-12 14:17:04 +0000

    MegaGera logos added

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ func main() {
 	// Routes
 	// http.HandleFunc("/upload", handlers.UploadImageHandler)
 	http.Handle("/megagoal/", http.StripPrefix("/megagoal/", http.FileServer(http.Dir("./static/megagoal"))))
+	http.Handle("/megagera/", http.StripPrefix("/megagera/", http.FileServer(http.Dir("./static/megagera"))))
 
 	// Start server
 	log.Println("Starting server on :8080...")
